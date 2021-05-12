9201deeaac9bb384634d86a0617f21ef3b082d7f
edgar <e.medina@xsensors.ai>
2021-05-12 13:42:43 +0000

    fine tunning for training purposes

--- dataloader/loader.py
+++ dataloader/loader.py
@@ -11,11 +11,11 @@ def Demo_Augmentation(image):
     # image = tf.image.random_contrast(image, lower=0.0, upper=1.0)
     return image
 
-def Get_database(name):
+def Get_database(name, shuffle_files=True, batch_size=16):
     # Load my dataset here
     # This functions were created in order to follow the full-pipeline format from TFDS-nightly.
-    train_ds = tfds.load(name, split='train', shuffle_files=True, batch_size=16)
-    test_ds = tfds.load(name, split='test', shuffle_files=True, batch_size=16)
+    train_ds = tfds.load(name, split='train', shuffle_files=shuffle_files, batch_size=batch_size)
+    test_ds = tfds.load(name, split='test', shuffle_files=shuffle_files, batch_size=batch_size)
     train_ds = train_ds.map(lambda data: (Demo_Augmentation(data["image"]), data["label"])).cache()
     test_ds = test_ds.map(lambda data: (Demo_Augmentation(data["image"]), data["label"])).cache()
     return train_ds, test_ds
--- main.py
+++ main.py
@@ -7,14 +7,14 @@ from environment import Testing as test
 
 # Config arguments or configuration file
 Epochs = 20
-classes=2
+classes=5 # 5 flowers classes
 eval_each_num_epochs = 5
 # ...
 
 # Training environment configuration
 train_stage = train.choose_methodology("simple")
 train_stage.loss_function = tools.Losses.SetCrossEntropy()
-train_stage.optimizer = tools.Optimizers.SetAdam(1e-3, Epochs)
+train_stage.optimizer = tools.Optimizers.SetAdam(1e-4, Epochs)
 # Testing environment configuration
 test_stage = test.choose_methodology("simple")
 test_stage.loss_function = tools.Losses.SetCrossEntropy()
@@ -48,8 +48,8 @@ for epoch in range(Epochs):
 
     print(
     f'Epoch {epoch + 1}, '
-    f'Loss: {train_stage.train_loss.result()}, '
-    f'Accuracy: {train_stage.train_accuracy.result() * 100}, '
+    f'Train Loss: {train_stage.train_loss.result()}, '
+    f'Train Accuracy: {train_stage.train_accuracy.result() * 100}, --- '
     f'Test Loss: {test_stage.test_loss.result()}, '
     f'Test Accuracy: {test_stage.test_accuracy.result() * 100}, '
     )
